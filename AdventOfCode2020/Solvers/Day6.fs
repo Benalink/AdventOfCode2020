ec3adbca6b00f0a7cb5423e2e4ae8c3593f49fee
Ben Alink <benalink@live.co.uk>
2020-12-06 13:17:35 +0000

    Simplified Day6 a little

--- AdventOfCode2020/Solvers/Day6.fs
+++ AdventOfCode2020/Solvers/Day6.fs
@@ -1,13 +1,13 @@
 ﻿namespace AdventOfCode2020.Solvers
 module Day6 =
+    let private parseGroups (input: string) = input.Split "\n\n"
+
     let Part1 (input: string) =
-        let groupInputs = input.Split "\n\n"
-        let distinctGroupInputs = groupInputs |> Seq.map (fun g -> g.Replace("\n", "") |> Seq.distinct)
-        distinctGroupInputs |> Seq.sumBy (fun dg -> dg |> Seq.length)
+        parseGroups input 
+        |> Seq.map (fun g -> g.Replace("\n", "") |> Seq.distinct)
+        |> Seq.sumBy (fun dg -> dg |> Seq.length)
 
     let Part2 (input: string) =
-        let groupInputs = input.Split "\n\n"
-        let groupedSets = groupInputs |> Seq.map (fun g -> g.Split('\n') |> Seq.map Set.ofSeq)
-        groupedSets
-        |> Seq.map (fun sets -> sets |> Seq.reduce Set.intersect)
-        |> Seq.sumBy(fun sets -> sets |> Seq.length)
+        parseGroups input
+        |> Seq.map (fun g -> g.Split('\n') |> Seq.map Set.ofSeq)
+        |> Seq.sumBy(fun sets -> sets |> Seq.reduce Set.intersect |> Seq.length)
