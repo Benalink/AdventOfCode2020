245c09b23e5588ee44a6eff611c0eee748df4138
Ben Alink <benalink@live.co.uk>
2020-12-07 14:45:29 +0000

    Added runner, inputs and Day7

---
+++ AdventOfCode2020/Program.fs
@@ -0,0 +1,30 @@
+﻿open AdventOfCode2020.Solvers
+
+let runSolver solver input =
+    printfn "Running Solver - %s" solver
+    match Array.ofSeq solver with
+    | [|'1'; 'a'|] -> Day1.Part1 input |> string
+    | [|'1'; 'b'|] -> Day1.Part2 input |> string
+    | [|'2'; 'a'|] -> Day2.Part1 input |> string
+    | [|'2'; 'b'|] -> Day2.Part2 input |> string
+    | [|'3'; 'a'|] -> Day3.Part1 input |> string
+    | [|'3'; 'b'|] -> Day3.Part2 input |> string
+    | [|'4'; 'a'|] -> Day4.Part1 input |> string
+    | [|'4'; 'b'|] -> Day4.Part2 input |> string
+    | [|'5'; 'a'|] -> Day5.Part1 input |> string
+    | [|'5'; 'b'|] -> Day5.Part2 input |> string
+    | [|'6'; 'a'|] -> Day6.Part1 input |> string
+    | [|'6'; 'b'|] -> Day6.Part2 input |> string
+    | _ -> failwith "Could not find a matching solver"
+
+
+
+[<EntryPoint>]
+let Main argv =
+    let result =
+        match argv with
+        | [|a; b|] -> runSolver a (System.IO.File.ReadAllText b)
+        | _ -> failwith "Invalid Arguments"
+    
+    printfn "Result: %s" result
+    0
---
+++ AdventOfCode2020/Solvers/Day7.fs
@@ -0,0 +1,67 @@
+﻿namespace AdventOfCode2020.Solvers
+
+module Day7 =
+    type private Bag = { Colour: string; Contents: (string * int) list }
+    
+    let removeBagSuffix (input: string) =
+        let a = input.Replace("bags.", "")
+        let b = a.Replace("bag.", "")
+        let c = b.Replace("bags", "")
+        c.Replace("bag", "")
+        
+    let private parseBag (bagDescription: string) =
+        let contentSplitter = "contain"
+        let contentSplit = bagDescription.Split contentSplitter
+        let bagColour = (removeBagSuffix contentSplit.[0]).Trim()
+        let contents = contentSplit.[1].Split ","
+        if contents.[0].Contains("no other bags") then
+            { Colour = bagColour; Contents = [] }
+        else
+        let parsedContents =
+            contents
+            |> Seq.map (fun c ->
+                let trimmed = c.Trim()
+                let spacerIndex = trimmed.IndexOf " "
+                ((removeBagSuffix (trimmed.Substring (spacerIndex + 1))).Trim(), int (trimmed.Substring(0, spacerIndex))))
+            |> Seq.toList
+
+        { Colour = bagColour; Contents = parsedContents }
+    
+    let Part1 (input: string) =
+        let rec coloursThatCanContainColour (bags: seq<Bag>) (colours: string list) =
+            let newColours =
+                bags
+                |> Seq.filter (fun b ->
+                               Set.intersect (Set.ofSeq (b.Contents |> Seq.map (fun (c, _) -> c))) (Set.ofList colours)
+                               |> Set.isEmpty
+                               |> not)
+                |> Seq.filter (fun b -> colours |> Seq.exists (fun c -> b.Colour = c) |> not)
+                |> Seq.map (fun b -> b.Colour)
+                |> Seq.toList
+                
+            match newColours with
+            | [] -> colours
+            | l -> coloursThatCanContainColour bags (colours @ l)
+
+        let parsedBags =
+            input.Split '\n'
+            |> Seq.map parseBag
+        
+        (coloursThatCanContainColour parsedBags ["shiny gold"] |> Seq.length) - 1
+        
+    let Part2 (input: string) =
+        let rec countBagsInBag (bag: Bag) (bagsMap: Map<string, Bag>) =
+            match bag.Contents with
+            | [] -> 1
+            | (c, i)::rest -> i * countBagsInBag (bagsMap.[c]) bagsMap + countBagsInBag { bag with Contents = rest } bagsMap
+        
+        let parsedBags =
+            input.Split '\n'
+            |> Seq.map parseBag
+        
+        let bagMap =
+            parsedBags
+            |> Seq.map (fun b -> b.Colour, b) |> Map
+        
+        let shinyGoldBag = parsedBags |> Seq.find (fun b -> b.Colour = "shiny gold")
+        (countBagsInBag shinyGoldBag bagMap) - 1
