32f4fc4240e341756d9d1620a607ba1ebf3ce663
Ben Alink <benalink@live.co.uk>
2020-12-15 15:43:29 +0000

    Solved Day 12, fixed mixed line endings

--- AdventOfCode2020/Program.fs
+++ AdventOfCode2020/Program.fs
@@ -2,27 +2,31 @@
 
 let runSolver solver input =
     printfn "Running Solver - %s" solver
-    match Array.ofSeq solver with
-    | [|'1'; 'a'|] -> Day1.Part1 input |> string
-    | [|'1'; 'b'|] -> Day1.Part2 input |> string
-    | [|'2'; 'a'|] -> Day2.Part1 input |> string
-    | [|'2'; 'b'|] -> Day2.Part2 input |> string
-    | [|'3'; 'a'|] -> Day3.Part1 input |> string
-    | [|'3'; 'b'|] -> Day3.Part2 input |> string
-    | [|'4'; 'a'|] -> Day4.Part1 input |> string
-    | [|'4'; 'b'|] -> Day4.Part2 input |> string
-    | [|'5'; 'a'|] -> Day5.Part1 input |> string
-    | [|'5'; 'b'|] -> Day5.Part2 input |> string
-    | [|'6'; 'a'|] -> Day6.Part1 input |> string
-    | [|'6'; 'b'|] -> Day6.Part2 input |> string
-    | [|'7'; 'a'|] -> Day7.Part1 input |> string
-    | [|'7'; 'b'|] -> Day7.Part2 input |> string
-    | [|'8'; 'a'|] -> Day8.Part1 input |> string
-    | [|'8'; 'b'|] -> Day8.Part2 input |> string
-    | [|'9'; 'a'|] -> Day9.Part1 input |> string
-    | [|'9'; 'b'|] -> Day9.Part2 input |> string
-    | [|'0'; 'a'|] -> Day10.Part1 input |> string
-    | [|'0'; 'b'|] -> Day10.Part2 input |> string
+    match solver with
+    | "1a" -> Day1.Part1 input |> string
+    | "1b" -> Day1.Part2 input |> string
+    | "2a" -> Day2.Part1 input |> string
+    | "2b" -> Day2.Part2 input |> string
+    | "3a" -> Day3.Part1 input |> string
+    | "3b" -> Day3.Part2 input |> string
+    | "4a" -> Day4.Part1 input |> string
+    | "4b" -> Day4.Part2 input |> string
+    | "5a" -> Day5.Part1 input |> string
+    | "5b" -> Day5.Part2 input |> string
+    | "6a" -> Day6.Part1 input |> string
+    | "6b" -> Day6.Part2 input |> string
+    | "7a" -> Day7.Part1 input |> string
+    | "7b" -> Day7.Part2 input |> string
+    | "8a" -> Day8.Part1 input |> string
+    | "8b" -> Day8.Part2 input |> string
+    | "9a" -> Day9.Part1 input |> string
+    | "9b" -> Day9.Part2 input |> string
+    | "10a" -> Day10.Part1 input |> string
+    | "10b" -> Day10.Part2 input |> string
+    | "11a" -> Day11.Part1 input |> string
+    | "11b" -> Day11.Part2 input |> string
+    | "12a" -> Day12.Part1 input |> string
+    | "12b" -> Day12.Part2 input |> string
     | _ -> failwith "Could not find a matching solver"
 
 [<EntryPoint>]
---
+++ AdventOfCode2020/Solvers/Day12.fs
@@ -0,0 +1,65 @@
+﻿namespace AdventOfCode2020.Solvers
+module Day12 =
+    let inline private modulo n m =
+        let mod' = n % m
+        if sign mod' >= 0 then mod'
+        else abs m + mod'
+
+    let private parseN (n: char list) =
+            n |> List.toArray |> System.String |> int       
+
+    let Part1 (input: string) =
+        let directions = ['N'; 'E'; 'S'; 'W']        
+        let rec runInstructions (instructions: string list) (position: int*int) (direction: int) =
+            let (x, y) = position;
+            match instructions with
+            | [] -> position
+            | first :: rest ->
+                let fList = first |> Seq.toList
+                let forwardHandledList =
+                    match fList with
+                    | 'F'::n -> [directions.[direction]] @ n
+                    | _ -> fList
+                    
+                match (forwardHandledList) with
+                | 'N'::n -> runInstructions rest (x, y + parseN(n)) direction
+                | 'S'::n -> runInstructions rest (x, y - parseN(n)) direction
+                | 'E'::n -> runInstructions rest (x + parseN(n), y) direction
+                | 'W'::n -> runInstructions rest (x - parseN(n), y) direction
+                | 'L'::n -> runInstructions rest position (modulo (direction - parseN(n) / 90) 4)
+                | 'R'::n -> runInstructions rest position (modulo (direction + parseN(n) / 90) 4)
+                | _ -> failwith "Unsupported Instruction"
+
+        let parsedInput = input.Split '\n' |> List.ofArray
+        let (x, y) = runInstructions parsedInput (0, 0) 1
+        abs x + abs y
+
+    let rec private rotate90 (v: int*int) (n: int) =
+        let (x, y) = v
+        match n with
+        | 0 -> v
+        | i when i < 0 -> rotate90 (-y, x) (i + 1)
+        | i when i > 0 -> rotate90 (y, -x) (i - 1)
+        | _ -> failwith "wat"
+
+    let Part2 (input: string) =
+        let rec runInstructions (instructions: string list) (position: int*int) (waypoint: int*int) =
+            let (x, y) = position
+            let (wX, wY) = waypoint           
+            match instructions with
+            | [] -> position
+            | first :: rest ->
+                let fList = first |> Seq.toList                    
+                match (fList) with
+                | 'N'::n -> runInstructions rest position (wX, wY + parseN(n))
+                | 'S'::n -> runInstructions rest position (wX, wY - parseN(n))
+                | 'E'::n -> runInstructions rest position (wX + parseN(n), wY)
+                | 'W'::n -> runInstructions rest position (wX - parseN(n), wY)
+                | 'L'::n -> runInstructions rest position (rotate90 waypoint (-parseN(n) / 90))
+                | 'R'::n -> runInstructions rest position (rotate90 waypoint (parseN(n) / 90))
+                | 'F'::n -> runInstructions rest (x + wX * parseN n, y + wY * parseN n) waypoint
+                | _ -> failwith "Unsupported Instruction"
+        
+        let parsedInput = input.Split '\n' |> List.ofArray
+        let (x, y) = runInstructions parsedInput (0, 0) (10, 1)
+        abs x + abs y
